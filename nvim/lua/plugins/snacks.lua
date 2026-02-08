86df83b26d2b4e8cfc88b2191975933e3365333c
Dimitrios Lytras <dnlytras@gmail.com>
2026-02-08 21:06:47 +0200

    Refactor snacks.nvim configuration structure

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -1,27 +1,27 @@
 return {
-  {
-    "snacks.nvim",
-    keys = {
-      { "<leader>gi", function() Snacks.picker.gh_issue() end, desc = "GitHub Issues (open)" },
-      { "<leader>gI", function() Snacks.picker.gh_issue({ state = "all" }) end, desc = "GitHub Issues (all)" },
-      { "<leader>gp", function() Snacks.picker.gh_pr() end, desc = "GitHub PRs (open)" },
-      { "<leader>gP", function() Snacks.picker.gh_pr({ state = "all" }) end, desc = "GitHub PRs (all)" },
-    },
-    opts = {
-      gh = {},
-      picker = {
-        sources = {
-          explorer = {},
-        },
-      },
-      dashboard = {
-        formats = {
-          key = function(item)
-            return { { "[", hl = "special" }, { item.key, hl = "key" }, { "]", hl = "special" } }
-          end,
-        },
-        preset = {
-          header = [[
+    {
+        "snacks.nvim",
+        opts = {
+            gh = {},
+            picker = {
+                sources = {
+                    explorer = {
+                        layout = {
+                            layout = {
+                                position = "right",
+                            },
+                        },
+                    },
+                },
+            },
+            dashboard = {
+                formats = {
+                    key = function(item)
+                        return { { "[", hl = "special" }, { item.key, hl = "key" }, { "]", hl = "special" } }
+                    end,
+                },
+                preset = {
+                    header = [[
     _     _      _     _      _     _
     (c).-.(c)    (c).-.(c)    (c).-.(c)
     / ._. \      / ._. \      / ._. \
@@ -32,15 +32,15 @@ return {
    (.-./`-'\.-.)(.-./`-'\.-.)(.-./`-'\.-.)
     `-'     `-'  `-'     `-'  `-'     `-'
 ]],
+                },
+                sections = {
+                    { section = "header",       padding = 1 },
+                    { title = "MRU ",           file = vim.fn.fnamemodify(".", ":~") },
+                    { section = "recent_files", cwd = true,                          limit = 8, padding = 1 },
+                    { title = "Bookmarks" },
+                    { section = "keys" },
+                },
+            },
         },
-        sections = {
-          { section = "header", padding = 1 },
-          { title = "MRU ", file = vim.fn.fnamemodify(".", ":~") },
-          { section = "recent_files", cwd = true, limit = 8, padding = 1 },
-          { title = "Bookmarks" },
-          { section = "keys" },
-        },
-      },
     },
-  },
 }
